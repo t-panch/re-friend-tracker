f3d4d89732ca067dc66a62af3e7285f0c6b2daf7
thuva <panchthu@students.zhaw.ch>
2018-12-11 14:54:23 +0100

    server url

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "Thuvas Friends Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
